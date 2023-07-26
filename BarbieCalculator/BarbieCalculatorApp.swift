540a26130f9b24b9d903c18499f4dd0c8ff409f7
Mariola <mariolahullings@gmail.com>
2023-07-26 19:36:20 -0400

    Initial Commit

---
+++ BarbieCalculator/BarbieCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarbieCalculatorApp.swift
+//  BarbieCalculator
+//
+//  Created by Mariola Hullings on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct BarbieCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
